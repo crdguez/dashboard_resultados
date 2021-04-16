b0bd198e35d11bbf511a1e19e18f46f7256145bf
Crdguez <31032709+crdguez@users.noreply.github.com>
2021-04-16 10:52:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 from io import StringIO
 
 url='https://gitlab.com/api/v4/projects/16754108/repository/files/importado1.csv/raw'
-url='https://gitlab.com/api/v4/projects/8982377/repository/files/importado1.csv/raw&private_token='+st.secrets["TOKEN"]
+url='https://gitlab.com/api/v4/projects/8982377/repository/files/importado1.csv/raw?ref="main"&private_token="'+st.secrets["TOKEN"]+'"
 st.write(url)
 df = pd.read_csv(StringIO(requests.get(url).text))
 
