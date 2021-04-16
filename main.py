fa01fe1ef59cde9ef331d9129dedc28aef2c4102
Crdguez <31032709+crdguez@users.noreply.github.com>
2021-04-16 10:01:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import streamlit as st
+import requests
+import requests
+from io import StringIO
+
+url='https://gitlab.com/api/v4/projects/16754108/repository/files/importado1.csv/raw'
+df = pd.read_csv(StringIO(requests.get(url).text))
 
 # Everything is accessible via the st.secrets dict:
 
 st.title("Resultados")
 
 st.write("Usuario:", st.secrets["USUARIO"])
+st.dataframe(df)
 
 
