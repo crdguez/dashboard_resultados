6febf3abf553a8344ceb35f6d328bc72482df6ed
Crdguez <31032709+crdguez@users.noreply.github.com>
2021-04-16 11:30:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,15 @@ if key == st.secrets["USUARIO"] :
   #url='https://gitlab.com/api/v4/projects/8982377/repository/files/importado1.csv/raw?ref=master&private_token='+st.secrets["TOKEN"]
   st.write(url)
   df = pd.read_csv(StringIO(requests.get(url).text))
+  
+  pre_actilla = pd.read_csv(StringIO(requests.get(url).text), index_col=False, encoding='utf-8')
+  pre_actilla = pre_actilla.drop([col for col in pre_actilla if col.startswith('Unna')], axis=1)
+  pre_actilla = pre_actilla.drop("Nº MNS", axis = 1)
+  pre_actilla = pd.melt(pre_actilla, id_vars=["Nº","Apellidos, Nombre"], var_name="Asignatura", value_name="Nota")
+  pre_actilla = pre_actilla[pre_actilla['Nota'].notna()]
+  pre_actilla = pre_actilla.copy()
+  pre_actilla['Eval'] = eval
+  pre_actilla.Asignatura=pre_actilla.Asignatura.str.replace('\n', ' ')
 
   # Everything is accessible via the st.secrets dict:
 
@@ -19,6 +28,7 @@ if key == st.secrets["USUARIO"] :
 
   st.write("Usuario:", st.secrets["USUARIO"])
   st.dataframe(df)
+  st.dataframe(pre_actilla)
   
 else :
   st.write('Para acceder a los datos tienes que introducir una clave de acceso correcta')
