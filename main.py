e6b0a9316111bb5b91be0762c141290e38a1b337
Crdguez <31032709+crdguez@users.noreply.github.com>
2021-04-16 11:22:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import requests
 import pandas as pd
 from io import StringIO
 
-key = ''
+
+key = st.text_input('clave de acceso:') 
+
 if key == st.secrets["USUARIO"] :
   url='https://gitlab.com/api/v4/projects/16754108/repository/files/importado1.csv/raw'
   #url='https://gitlab.com/api/v4/projects/8982377/repository/files/importado1.csv/raw?ref=master&private_token='+st.secrets["TOKEN"]
@@ -19,6 +21,6 @@ if key == st.secrets["USUARIO"] :
   st.dataframe(df)
   
 else :
-  st.write('Para acceder a los datos tienes que introducir una clave de acceso')
+  st.write('Para acceder a los datos tienes que introducir una clave de acceso correcta')
 
-key = st.text_input('clave de acceso:')  
+
