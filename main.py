2c31aa32411d2cec52839fe7d9114776fac23bf4
Crdguez <31032709+crdguez@users.noreply.github.com>
2021-04-16 10:11:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 from io import StringIO
 
 url='https://gitlab.com/api/v4/projects/16754108/repository/files/importado1.csv/raw'
-#url='https://gitlab.com/api/v4/projects/16754108/repository/files/importado1.csv/raw'
+#url='https://gitlab.com/api/v4/projects/16754108/repository/files/importado1.csv/raw&private_token='+st.secrets["TOKEN"]
 df = pd.read_csv(StringIO(requests.get(url).text))
 
 # Everything is accessible via the st.secrets dict:
